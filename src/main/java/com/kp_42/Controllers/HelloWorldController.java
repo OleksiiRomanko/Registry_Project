cd4503c1fb3febe9496dc279edb08e80f468bc90
Sasha <granium90@gmail.com>
2016-03-05 19:03:28 +0200

    template

---
+++ src/main/java/com/kp_42/Controllers/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.kp_42.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by user on 05.03.2016.
+ */
+
+@Controller
+public class HelloWorldController {
+
+
+    @RequestMapping("/hello")
+    public String hello(ModelMap map){
+
+
+
+        return "Hello";
+    }
+
+}
---
+++ target/Registry/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 05.03.2016
+  Time: 18:44
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index page</title>
+</head>
+<body>
+
+Press <a href="/hello">this</a> link
+
+</body>
+</html>
